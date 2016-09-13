0e1ab1c7ca20708f68e30d7dcf6a4095c9b2d85b
Kenichi Nakamura <nkkn1008@gmail.com>
2016-09-14 07:07:26 +0900

    Scaffold a migration to create the initial set of tables for model.
    Command:
    Add-Migration MyFirstMigration

---
+++ SpikeLocalSQLite/Migrations/20160913220213_MyFirstMigration.Designer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using SpikeLocalSQLite;
+
+namespace SpikeLocalSQLite.Migrations
+{
+    [DbContext(typeof(BloggingContext))]
+    [Migration("20160913220213_MyFirstMigration")]
+    partial class MyFirstMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
+
+            modelBuilder.Entity("SpikeLocalSQLite.Blog", b =>
+                {
+                    b.Property<int>("BlogId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("BlogId");
+
+                    b.ToTable("Blogs");
+                });
+
+            modelBuilder.Entity("SpikeLocalSQLite.Post", b =>
+                {
+                    b.Property<int>("PostId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BlogId");
+
+                    b.Property<string>("Content");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("PostId");
+
+                    b.HasIndex("BlogId");
+
+                    b.ToTable("Posts");
+                });
+
+            modelBuilder.Entity("SpikeLocalSQLite.Post", b =>
+                {
+                    b.HasOne("SpikeLocalSQLite.Blog", "Blog")
+                        .WithMany("Posts")
+                        .HasForeignKey("BlogId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
