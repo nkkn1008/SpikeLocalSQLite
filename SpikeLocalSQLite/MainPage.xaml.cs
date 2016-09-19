d72378c9292cd135e92b33953cf7678738f28d72
Kenichi Nakamura <nkkn1008@gmail.com>
2016-09-19 19:00:10 +0900

    Entity 間の関係性を表現するサンプルを追加した

--- SpikeLocalSQLite/MainPage.xaml.cs
+++ SpikeLocalSQLite/MainPage.xaml.cs
@@ -83,5 +83,27 @@ namespace SpikeLocalSQLite
                 Blogs.ItemsSource = blogs;
             }
         }
+
+        private void AddWithReference_Click(object sender, RoutedEventArgs e)
+        {
+            using (var context = new BloggingContext())
+            {
+                var blog = new Blog
+                {
+                    Url = "http://blogs.msdn.com/dotnet",
+                    Posts = new List<Post>
+                    {
+                        new Post { Title = "Intro to C#" },
+                        new Post { Title = "Intro to VB.NET" },
+                        new Post { Title = "Intro to F#" }
+                    }
+                };
+
+                context.Blogs.Add(blog);
+                context.SaveChanges();
+                Blogs.ItemsSource = context.Blogs.ToList();
+            }
+
+        }
     }
 }
